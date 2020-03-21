fb7cf8fc6afab9e39098fc43d573d01ea1a500cd
CristianPeralta <cbperaltasegura@gmail.com>
2020-03-21 07:12:03 -0500

    add state and methods to show or close modal and backdrop

--- src/App.js
+++ src/App.js
@@ -6,13 +6,25 @@ import Backdrop from "./components/Backdrop/Backdrop";
 import List from "./components/List/List";
 
 class App extends Component {
+  state = {
+    modaIslOpen: false,
+  }
+
+  showModal = () => {
+    console.log('show click', this.state.modaIslOpen)
+    this.setState({ modaIslOpen: true });
+  }
+
+  closeModal = () => {
+    this.setState({ modaIslOpen: false });
+  }
   render() {
     return (
       <div className="App">
         <h1>React Animations</h1>
-        <Modal />
-        <Backdrop />
-        <button className="Button">Open Modal</button>
+        <Modal show={this.state.modaIslOpen} closed={this.closeModal} />
+        <Backdrop show={this.state.modaIslOpen} />
+        <button className="Button" onClick={this.showModal}>Open Modal</button>
         <h3>Animating Lists</h3>
         <List />
       </div>
