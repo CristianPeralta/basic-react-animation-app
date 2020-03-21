80c97057a530b23cebb95c6bae25abf21c9073f1
CristianPeralta <cbperaltasegura@gmail.com>
2020-03-21 08:07:19 -0500

    add toggle button and red div

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import List from "./components/List/List";
 class App extends Component {
   state = {
     modaIslOpen: false,
+    showBlock: false,
   }
 
   showModal = () => {
@@ -21,6 +22,15 @@ class App extends Component {
     return (
       <div className="App">
         <h1>React Animations</h1>
+        <button className="Button" onClick={() => this.setState(prevstate => ({showBlock: !prevstate.showBlock}))} >Toggle</button>
+        {this.state.showBlock ? <div
+          style={{
+            background: 'red',
+            width: 100,
+            height: 100,
+            margin: 'auto'
+          }} ></div> : null}
+        <br/>
         {this.state.modaIslOpen ? <Modal show={this.state.modaIslOpen} closed={this.closeModal} /> : null }
         {this.state.modaIslOpen ? <Backdrop show={this.state.modaIslOpen} /> : null }
         <button className="Button" onClick={this.showModal}>Open Modal</button>
